1d639f3148b05b5878b4a00dd18553eeaecd11b9
Aaryan <aryandhiman540@gmail.com>
2026-02-11 00:00:44 +0530

    Added rectangle area program

---
+++ basics/rectangle_area.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int l,b, area;
+
+    printf("Enter the Length :");
+    scanf("%d" ,&l);
+
+    printf("Enter the Breadth :");
+    scanf("%d" ,&b);
+
+    area = l*b;
+    printf("Area of Rectangle = %d" , area);
+
+    return 0;
+}
