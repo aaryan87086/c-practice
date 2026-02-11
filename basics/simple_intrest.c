a499a731a44e6931affb744c7175da9485f0bac7
Aaryan <aryandhiman540@gmail.com>
2026-02-11 21:49:37 +0530

    :wq Program to calculate Simple Intrest

---
+++ basics/simple_intrest.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    float P,R,T,SI ;
+
+    printf("Enter value of P :");
+    scanf("%f",&P);
+
+    printf("Enter value of R :");
+    scanf("%f",&R);
+
+    printf("Enter value of T :");
+    scanf("%f",&T);
+
+    SI = (P*R*T)/100;
+    {
+    printf("The Simple Intrest is = %f" , SI);
+    }
+    return 0;
+}
