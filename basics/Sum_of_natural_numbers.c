8babeb311519e00ddff1111c3a6e5e2d33b33678
Aaryan <aryandhiman540@gmail.com>
2026-02-11 22:43:30 +0530

    Sum of natural numbers

---
+++ basics/Sum_of_natural_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int  n = 1, sum = 0;
+
+    while (n<=10)
+    
+    {
+        sum = sum +n;
+        n++;
+    }
+
+    printf("The Sum of number is %d" , sum);
+    return 0;
+
+}
